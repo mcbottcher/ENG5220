cf760e8d6734ea369fe41865a69380bbca675e2e
Mikkel Caschetto <59874862+mcbottcher@users.noreply.github.com>
2020-02-05 15:33:24 +0000

    Update RingBuff.h

--- code/RingBuff.h
+++ code/RingBuff.h
@@ -1,17 +1,15 @@
-
-
-
 class RingBuff {
 
- 
     protected:
         int buf_size; 
+ 
     public:
-        
         double* buffer;
         unsigned offset;
+ 
         RingBuff(int buf_size) ; 
         void process(double sample);
         void reset_buffer();
         ~RingBuff();    
+ 
 };
