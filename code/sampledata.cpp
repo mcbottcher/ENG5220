60b223f4128372fb96006e91f0f3209adf12ccf3
mcbottcher <mcbottcher@gmail.com>
2020-02-29 10:05:19 +0000

    added MPU module to main sampling code

--- code/sampledata.cpp
+++ code/sampledata.cpp
@@ -1,23 +1,48 @@
 #include <unistd.h>
-#include <stdio.h> 
+#include <stdio.h>
+#include <stdint.h> 
 
 #include "cppTimer/CppTimer.h"
+#include "MPU6050/MPU6050.h"
+#include "MPU6050/I2Cdev.h"
+
+#define MPU6050_I2C_ADDRESS 0x69
+
+//accelerometer and gyroscope values
+static int16_t ax, ay, az;
+static int16_t gx, gy, gz;
+
+MPU6050 motionSensor(MPU6050_I2C_ADDRESS);
+
+
+void sampleSensors(){
+
+	motionSensor.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+}
 
 
 class Ticker : public CppTimer{
 
 	void timerEvent{
-				
-		//insert function calls here...
-		
+		sampleSensors();
+		insertDataToBuff();
 	}
-
 }
 
 
+
+/* **************************************************************************** */
+
 int main(){
+
+	motionSensor.initialize();	
+	printf(motionSensor.testConnection() ? "MPU6050 connection successful\r\n" : "MPU6050 connection failed\r\n");
 
 	Ticker sampleTimer;
 	sampleTimer.start(100000000); //time in ns == 100ms
+	printf("Timer Setup!\n\r");
+
+	
 
 }
